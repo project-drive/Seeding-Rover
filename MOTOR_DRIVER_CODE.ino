810eaa3decb0084b29610297dff1e87fabc2c520
AbhishekKadu <52886378+AbhishekKadu@users.noreply.github.com>
2019-07-21 14:27:28 +0530

    Create MOTOR_DRIVER_CODE.ino
    Motor driver code to move the rover forward and backward.

---
+++ MOTOR_DRIVER_CODE.ino
@@ -0,0 +1,41 @@
+int m1a = 10;
+int m1b = 11;
+int m2a = 12;
+int m2b = 13;
+char val;
+
+void setup() 
+{  
+pinMode(m1a, OUTPUT);  // Digital pin 10 set as output Pin
+pinMode(m1b, OUTPUT);  // Digital pin 11 set as output Pin
+pinMode(m2a, OUTPUT);  // Digital pin 12 set as output Pin
+pinMode(m2b, OUTPUT);  // Digital pin 13 set as output Pin
+Serial.begin(9600);
+}*
+
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+  val = Serial.read();
+  Serial.println(val);
+  }
+
+  if( val == 'F') // Forward
+    {
+      digitalWrite(m1a, HIGH);
+      digitalWrite(m1b, LOW);
+      digitalWrite(m2a, HIGH);
+      digitalWrite(m2b, LOW); 
+    }
+  else if(val == 'B') // Backward
+    {
+      digitalWrite(m1a, LOW);
+      digitalWrite(m1b, HIGH);
+      digitalWrite(m2a, LOW);
+      digitalWrite(m2b, HIGH); 
+    }
+  
+    
+}
+//only forward and backward
